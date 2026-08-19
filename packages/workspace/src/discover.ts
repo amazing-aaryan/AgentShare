c601060880cd1c2f5c235a1f6533d65da4617c40
Aaryan Srivastava <aaryansr@umich.edu>
2026-08-19 11:11:05 +0300

    chore: remove obsolete workspace package copy

--- packages/workspace/src/discover.ts
+++
@@ -1,8 +0,0 @@
-import { realpath, stat } from "node:fs/promises";
-
-export async function discoverWorkspaceRoot(cwd: string): Promise<string> {
-  const root = await realpath(cwd);
-  const metadata = await stat(root);
-  if (!metadata.isDirectory()) throw new Error("AgentShare workspace root must be a directory");
-  return root;
-}
